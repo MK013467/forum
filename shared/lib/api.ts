f5bfba7c65ce79513c1f6bddd49ad19d68f4af15
Mk013467 <minsuk603@gmail.com>
2026-03-31 01:37:20 +0900

    frontend file changed

--- shared/lib/api.ts
+++ shared/lib/api.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance} from "axios";
 const url = process.env.NODE_ENV === "development"? "http://localhost:3000": "https://forum-app-production.up.railway.app";
 export const api:AxiosInstance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://forum-app-production.up.railway.app",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
