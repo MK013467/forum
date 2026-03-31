40e69d8a203ac48a0adf9c4a2e6999c5b2ae31b9
Mk013467 <minsuk603@gmail.com>
2026-03-31 20:47:45 +0900

    changed url

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.DEV
+const baseURL = import.meta.env.VITE_ENV === "DEV"
   ? "http://localhost:3000"
   : "https://forum-app-production.up.railway.app";
 export const api = axios.create({
