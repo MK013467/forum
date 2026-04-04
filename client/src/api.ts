3b818c492b68590c5b706d0b48982ad3dfa922aa
Mk013467 <minsuk603@gmail.com>
2026-04-05 00:29:37 +0900

    api changed

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_ENV === "DEV"
   ? "http://localhost:3000":"https://forum-app-production.up.railway.app";
-export const api = axios.create({
-    baseURL: baseURL,
+
+  export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
     headers: {
-        'Content-Type': 'application/json',
-      },
+      "Content-Type": "application/json",
+    },
   });
-
   api.interceptors.response.use(
     (response) => {
         return {
