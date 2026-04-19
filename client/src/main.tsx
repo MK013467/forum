9b440f3d61541c590a62831c390c5c256f1813ac
Mk013467 <minsuk603@gmail.com>
2026-04-19 22:56:48 +0900

    Working on recommendation Service

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -20,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
 
   <StrictMode>
     <ErrorBoundary FallbackComponent={RootErrorPage}>
+      
       <ToastContainer 
         position='bottom-center'
         autoClose={2000}
