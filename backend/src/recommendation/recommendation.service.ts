af677bdf3cadef87d6df42648dc5b759c492b93d
Mk013467 <minsuk603@gmail.com>
2026-04-26 23:42:09 +0900

    worked on getEmbedding method using Huggingface hf model

--- backend/src/recommendation/recommendation.service.ts
+++ backend/src/recommendation/recommendation.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { HfInference } from '@huggingface/inference';
 
 @Injectable()
-export class RecommendationService {}
+export class RecommendationService {
+    private hf: HfInference;
+    private model = 'sentence-transformers/all-MiniLM-L6-v2';
+  
+    constructor() {
+      this.hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
+    }
+    
+    // Text to embedding
+    async getEmbedding(text: string): Promise<number[]> {
+      const result = await this.hf.featureExtraction({
+        model: this.model,
+        inputs: text,
+      });
+      return result as number[];
+    }
+}
